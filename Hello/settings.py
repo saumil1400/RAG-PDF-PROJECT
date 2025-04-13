559df3c6a5d33ddb240996ed8624e182b1ecf8ca
Saumil Gupta <saumilgupta1400@gmail.com>
2025-04-13 10:08:02 +0530

    FIXED CROSS ORIGIN REQUESTS

--- Hello/settings.py
+++ Hello/settings.py
@@ -133,7 +133,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ========== Custom Additions Below ==========
 
 # CORS settings
+# CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Allow iframe embedding from any origin
 X_FRAME_OPTIONS = 'ALLOWALL'
